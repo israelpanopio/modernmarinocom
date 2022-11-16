8d452dcd6bb776d089b0072c31d9ff0ed3682a7e
Israel Garcia Panopio <75904292+israelpanopio@users.noreply.github.com>
2022-11-16 19:43:26 +0800

    bestcoding google ads

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -4,6 +4,15 @@ import Head from 'next/head';
 import Script from "next/script";
 
 const Layout = ({ children }) => {
+  useEffect(() => {
+    var ads = document.getElementsByClassName("adsbygoogle").length;
+    for (var i = 0; i < ads; i++) {
+      try {
+        (adsbygoogle = window.adsbygoogle || []).push({});
+      } catch (e) { }
+    }
+}, []);
+
   return (
     <>
       <Script
@@ -17,6 +26,7 @@ const Layout = ({ children }) => {
         <meta name="Modern Marino" content="by Israel Panopio" />
         <link rel="icon" href="/favicon.ico" />
         <link href="https://fonts.googleapis.com/css2?family=Goldman:wght@700&family=Josefin+Sans:wght@700&family=Lemonada&family=Montserrat&display=swap" rel="stylesheet"></link>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
       </Head>
       
       {children}
