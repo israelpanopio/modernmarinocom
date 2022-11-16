3f738d4e2c05656307374bb99272ef0f8cc575fe
Israel Garcia Panopio <75904292+israelpanopio@users.noreply.github.com>
2022-11-16 22:54:47 +0800

    hope finally works

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,8 @@
 import type { AppProps } from 'next/app'
 import GlobalStyle from '../components/globalstyles'
 import { Layout } from '../components'
-import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    var ads = document.getElementsByClassName("adsbygoogle").length;
-    for (var i = 0; i < ads; i++) {
-      try {
-        (adsbygoogle = window.adsbygoogle || []).push({});
-      } catch (e) { }
-    }
-}, []);
-
   return (
     <>
       <GlobalStyle />
