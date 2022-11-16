37899792b257ac9372d15571409040c024667c69
Israel Garcia Panopio <75904292+israelpanopio@users.noreply.github.com>
2022-11-16 19:29:40 +0800

    asdada

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -6,18 +6,18 @@ import Script from "next/script";
 const Layout = ({ children }) => {
   return (
     <>
-
+      <Script
+        id="Adsense-id"
+        data-ad-client="ca-pub-4841621324421656"
+        strategy="beforeInteractive"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+      />
       <Head>
         <title>Modern Marino</title>
         <meta name="Modern Marino" content="by Israel Panopio" />
         <link rel="icon" href="/favicon.ico" />
         <link href="https://fonts.googleapis.com/css2?family=Goldman:wght@700&family=Josefin+Sans:wght@700&family=Lemonada&family=Montserrat&display=swap" rel="stylesheet"></link>
-        <Script
-        id="Adsense-id"
-        data-ad-client="ca-pub-4841621324421656"
-        strategy="beforeInteractive"
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-      /></Head>
+      </Head>
       
       {children}
       <Footer />
