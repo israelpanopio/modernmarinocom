7fc94fd490dcb35413b971524f4b71a35b437ef5
Israel Garcia Panopio <75904292+israelpanopio@users.noreply.github.com>
2022-11-16 14:53:24 +0800

    googleads

--- components/HeadContent.jsx
+++ components/HeadContent.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import Head from 'next/head';
+import Script from "next/script";
 
 const HeadContent = () => {
-  return (
+  return ( <>
+    <Script data-ad-client="ca-pub-4841621324421656" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></Script>
+
     <Head>
     <title>Modern Marino</title>
     <meta name="Modern Marino" content="by Israel Panopio" />
     <link rel="icon" href="/favicon.ico" />
     <link href="https://fonts.googleapis.com/css2?family=Goldman:wght@700&family=Josefin+Sans:wght@700&family=Lemonada&family=Montserrat&display=swap" rel="stylesheet"></link>
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
   </Head>
-  )
+  </>)
 }
 
 export default HeadContent
